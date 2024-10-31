786400f1441c4b97bdcae4482c012aab0fb2ff4c
naraabinnadhikari <narayanabin223@gmail.com>
2024-10-31 20:40:45 +0545

    10. not-found file & page and notFound function.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='flex items-center justify-center h-screen' >
+        <div>
+        <h2 className='text-center p-4'>❌ Sorry, Page not found. </h2>
+        <p> Could not find requested Resource.</p>
+        </div>
+    </div>
+  )
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { notFound } from 'next/navigation';
+
 
 export default function ReviewDetails( { params }: { params: {
     productId: string;
     reviewId: string
  };
 } ) {
+  if(parseInt(params.reviewId) > 1000) {
+    notFound()
+  }
   return (
     <div>
         <h3>Review {params.reviewId} for Product {params.productId} </h3>
