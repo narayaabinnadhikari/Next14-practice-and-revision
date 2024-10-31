9bba75b586e555ff69bdde190b5048116c3ecdaf
naraabinnadhikari <narayanabin223@gmail.com>
2024-10-31 20:20:48 +0545

    9. catch-all-segment [...name] & user-defined catch-all [[ ...name]]

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Docs( {params} : {
+    params: {
+        slug: string[];
+    };
+}) {
+    if(params.slug?.length === 2 ) {
+        return <h2>Viewing Docs for feature {params.slug[0]} and concept {params.slug[1]}. </h2>
+    } else if (params.slug?.length === 1) {
+        return <h2>Viewing Docs for feature {params.slug[0]}.</h2>
+
+    }
+  return (
+    <div>Documentation Home Page.</div>
+  )
+}
